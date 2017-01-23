c77e513197f1aff195b9a1006d9e9583042ab725
cbsuque <cedrick.busque@polymtl.ca>
2017-01-23 17:49:14 -0500

    Initial commit--

---
+++ app/src/main/java/com/example/cedri/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cedri.myapplication;
+
+
+import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+//import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       // final TextView likeBoutonText = (TextView) findViewById(R.id.textView2)
+    }
+}
